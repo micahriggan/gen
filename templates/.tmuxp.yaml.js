f6d0ca3a7011300d1bce1eb8823b247e8505bff7
Micah Riggan <micahriggan@gmail.com>
2024-09-08 07:19:54 -0400

    updating tmuxp

--- templates/.tmuxp.yaml.js
+++ templates/.tmuxp.yaml.js
@@ -3,7 +3,7 @@ module.exports =
 session_name: coding
 start_directory: ./
 windows:
-  - window_name: all
+  - window_name: project
     layout: main-vertical
     options:
       main-pane-width: 50%
@@ -13,29 +13,3 @@ windows:
       - shell_command:
       - shell_command:
       - shell_command:
-
-  - window_name: frontend
-    layout: main-vertical
-    options:
-      main-pane-width: 50%
-    start_directory: ./packages/frontend
-    panes:
-      - shell_command:
-        - vim
-      - shell_command:
-        - yarn dev
-      - shell_command:
-      - shell_command:
-
-  - window_name: contracts
-    layout: main-vertical
-    options:
-      main-pane-width: 50%
-    start_directory: ./packages/contracts
-    panes:
-      - shell_command:
-        - vim
-      - shell_command:
-      - shell_command:
-      - shell_command:
-`;
